3efc94195847d50a20ce26103cdbcaa2bda815e8
Dino Chiesa <dchiesa@google.com>
2022-01-04 18:35:25 -0800

    update antrun plugin
    also remove some lint from Java code

--- Readme.md
+++ Readme.md
@@ -1,20 +1,26 @@
 # Java Weighted Selector callout
 
 This directory contains the Java source code and pom.xml file required to
-compile a simple Java callout for Apigee Edge. The callout is performs a weighted random selection,
-based on inputs.
+compile a simple Java callout for Apigee. The callout is performs a weighted
+random selection, based on inputs.
 
 ## Building:
+
+You do not need to build this callout in order to use it.  You can build if you want to. 
 
 1. unpack (if you can read this, you've already done that).
 
 2. configure the build on your machine by loading the Apigee jars into your local cache
-  ```./buildsetup.sh```
+  ```
+  ./buildsetup.sh
+  ```
 
 2. Build with maven.
-  ```mvn clean package```
+  ```
+  mvn clean package
+  ```
 
-3. if you edit proxy bundles offline, copy the resulting jar file, available in  target/httpsig-edge-callout.jar to your apiproxy/resources/java directory.  If you don't edit proxy bundles offline, upload the jar file into the API Proxy via the Edge API Proxy Editor .
+3. if you edit proxy bundles offline, copy the resulting jar file, available in  target/apigee-weighted-selector-20220104.jar  to your apiproxy/resources/java directory.  If you don't edit proxy bundles offline, upload the jar file into the API Proxy via the Apigee API Proxy Editor .
 
 4. include an XML file for the Java callout policy in your
    apiproxy/resources/policies directory. It should look
@@ -25,19 +31,17 @@ based on inputs.
        <Property name="weights">0, 1, 1, 2, 3, 5, 8, 13, 21, 34</Property>
      </Properties>
      <ClassName>com.google.apigee.callout.WeightedSelectorCallout</ClassName>
-     <ResourceURL>java://edge-custom-weighted-selector-1.0.1.jar</ResourceURL>
+     <ResourceURL>java://apigee-custom-weighted-selector-20220104.jar</ResourceURL>
    </JavaCallout>
    ```
 
-5. use the Edge UI, or a command-line tool like pushapi (See
-   https://github.com/carloseberhardt/apiploy) or similar to
-   import the proxy into an Edge organization, and then deploy the proxy .
-   Eg,
-   ```./pushapi -v -d -o ORGNAME -e test -n weighted-selector bundle```
+5. use the Edge UI or a command-line tool to
+   import the proxy into an apigee organization, and then deploy the proxy .
 
 6. Use a client to generate and send http requests to the proxy. Eg,
-   ```curl -i http://ORGNAME-test.apigee.net/weighted-selector/t1```
-
+   ```
+   curl -i $endpoint/weighted-selector/t1
+   ```
 
 
 
@@ -45,11 +49,13 @@ based on inputs.
 
 - Apigee Edge expressions v1.0
 - Apigee Edge message-flow v1.0
-- Google Guava v20.0
+- Google Guava v30.0
 
 These jars must be available on the classpath for the compile to
 succeed. The buildsetup.sh script will download these files for
 you automatically, and will insert them into your maven cache.
+
+They are included in the Apigee runtime.
 
 ## Notes
 
@@ -57,29 +63,10 @@ There is one callout class, com.google.apigee.callout.WeightedSelectorCallout,
 which does the weighted selection and then inserts the computed bucket value into a context variable.
 
 
-## Example Usage
-
-```
-$ curl -i https://ORGNAME-test.apigee.net/nanotime/t1
-HTTP/1.1 200 OK
-Date: Tue, 10 Nov 2015 19:41:39 GMT
-Content-Type: application/json
-Content-Length: 161
-Connection: keep-alive
-Server: Apigee Router
-
-{
-  "message" : "ok",
-  "system.uuid" : "ac4185b1-f5ee-4997-9448-00399715f2f4",
-  "bucket" : "8",
-  "now" : "Thu, 3 Aug 2017 18:52:49 UTC"
-}
-
-```
 
 ## LICENSE
 
-This material is [Copyright (c) 2017 Google LLC](NOTICE).
+This material is [Copyright (c) 2017-2022 Google LLC](NOTICE).
 and is licensed under the Apache 2.0 license. See the [LICENSE](LICENSE) file.
 
 
--- callout/src/main/java/com/google/apigee/callout/WeightedSelectorCallout.java
+++ callout/src/main/java/com/google/apigee/callout/WeightedSelectorCallout.java
@@ -2,7 +2,7 @@
 //
 // This is the source code for a Java callout for Apigee Edge.
 //
-// Copyright 2018 Google LLC.
+// Copyright 2018-2022 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,107 +25,117 @@ import com.apigee.flow.execution.ExecutionResult;
 import com.apigee.flow.execution.spi.Execution;
 import com.apigee.flow.message.MessageContext;
 import com.google.common.base.CharMatcher;
-import com.google.common.base.Joiner;
-import com.google.common.base.Predicate;
 import com.google.common.base.Splitter;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Iterables;
-import java.util.Collection;
-import java.util.function.Function;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import org.apache.commons.lang.exception.ExceptionUtils;
 
 public class WeightedSelectorCallout implements Execution {
-    private final static String varprefix= "wrs_";
-    private LoadingCache<String, WeightedRandomSelector> selectorCache;
-    private static String varName(String s) { return varprefix + s;}
-    private final static Splitter splitter = Splitter.on(CharMatcher.anyOf(" ,")).trimResults().omitEmptyStrings();
-    private final static String variableReferencePatternString = "(.*?)\\{([^\\{\\} ]+?)\\}(.*?)";
-    private final static Pattern variableReferencePattern = Pattern.compile(variableReferencePatternString);
-    private Map properties;
-    public WeightedSelectorCallout(Map properties) {
-        this.properties = properties;
-        this.selectorCache = CacheBuilder.newBuilder()
+  private static final String varprefix = "wrs_";
+  private LoadingCache<String, WeightedRandomSelector> selectorCache;
+
+  private static String varName(String s) {
+    return varprefix + s;
+  }
+
+  private static final Splitter splitter =
+      Splitter.on(CharMatcher.anyOf(" ,")).trimResults().omitEmptyStrings();
+  private static final String variableReferencePatternString = "(.*?)\\{([^\\{\\} ]+?)\\}(.*?)";
+  private static final Pattern variableReferencePattern =
+      Pattern.compile(variableReferencePatternString);
+  private Map properties;
+
+  public WeightedSelectorCallout(Map properties) {
+    this.properties = properties;
+    this.selectorCache =
+        CacheBuilder.newBuilder()
             .concurrencyLevel(4)
-            //.weakKeys()
+            // .weakKeys()
             .maximumSize(1048000)
             .expireAfterAccess(10, TimeUnit.MINUTES)
-            .build(new CacheLoader<String, WeightedRandomSelector>() {
-                    public WeightedRandomSelector load(String weightsConfig) {
-                        Iterable<Integer> weights = Iterables.transform(splitter.split(weightsConfig), s -> Integer.valueOf(s));
-                        return new WeightedRandomSelector(weights);
-                    }
-                }
-                );
+            .build(
+                new CacheLoader<String, WeightedRandomSelector>() {
+                  public WeightedRandomSelector load(String weightsConfig) {
+                    Iterable<Integer> weights =
+                        Iterables.transform(splitter.split(weightsConfig), s -> Integer.valueOf(s));
+                    return new WeightedRandomSelector(weights);
+                  }
+                });
+  }
+
+  private String getWeights(MessageContext msgCtxt) throws Exception {
+    String weights = (String) this.properties.get("weights");
+    if (weights == null || weights.equals("")) {
+      throw new IllegalStateException("weights is not specified or is empty.");
+    }
+    weights = (String) resolvePropertyValue(weights, msgCtxt);
+    if (weights == null || weights.equals("")) {
+      throw new IllegalStateException("weights is null or empty.");
+    }
+    return weights;
+  }
+
+  private boolean getDebug() {
+    String wantDebug = (String) this.properties.get("debug");
+    boolean debug = (wantDebug != null) && Boolean.parseBoolean(wantDebug);
+    return debug;
+  }
+
+  // If the value of a property contains a pair of curlies,
+  // eg, {apiproxy.name}, then "resolve" the value by de-referencing
+  // the context variable whose name appears between the curlies.
+  private String resolvePropertyValue(String spec, MessageContext msgCtxt) {
+    Matcher matcher = variableReferencePattern.matcher(spec);
+    StringBuffer sb = new StringBuffer();
+    while (matcher.find()) {
+      matcher.appendReplacement(sb, "");
+      sb.append(matcher.group(1));
+      Object v = msgCtxt.getVariable(matcher.group(2));
+      if (v != null) {
+        sb.append((String) v);
+      }
+      sb.append(matcher.group(3));
+    }
+    matcher.appendTail(sb);
+    return sb.toString();
+  }
+
+  protected static String getStackTraceAsString(Throwable t) {
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    t.printStackTrace(pw);
+    return sw.toString();
+  }
+
+  public ExecutionResult execute(final MessageContext msgCtxt, final ExecutionContext execContext) {
+    try {
+      String weightsConfig = getWeights(msgCtxt);
+      WeightedRandomSelector wrs = selectorCache.get(weightsConfig);
+      // set a variable.
+      msgCtxt.setVariable(varName("bucket"), Integer.toString(wrs.select()));
+    } catch (Exception e) {
+      if (getDebug()) {
+        System.out.println(getStackTraceAsString(e));
+      }
+      String error = e.toString();
+      msgCtxt.setVariable(varName("exception"), error);
+      int ch = error.lastIndexOf(':');
+      if (ch >= 0) {
+        msgCtxt.setVariable(varName("error"), error.substring(ch + 2).trim());
+      } else {
+        msgCtxt.setVariable(varName("error"), error);
+      }
+      msgCtxt.setVariable(varName("stacktrace"), getStackTraceAsString(e));
+      return ExecutionResult.ABORT;
     }
 
-    private String getWeights(MessageContext msgCtxt) throws Exception {
-        String weights = (String) this.properties.get("weights");
-        if (weights == null || weights.equals("")) {
-             throw new IllegalStateException("weights is not specified or is empty.");
-        }
-        weights = (String) resolvePropertyValue(weights, msgCtxt);
-        if (weights == null || weights.equals("")) {
-            throw new IllegalStateException("weights is null or empty.");
-        }
-        return weights;
-    }
-
-    private boolean getDebug() {
-        String wantDebug = (String) this.properties.get("debug");
-        boolean debug = (wantDebug != null) && Boolean.parseBoolean(wantDebug);
-        return debug;
-    }
-
-    // If the value of a property contains a pair of curlies,
-    // eg, {apiproxy.name}, then "resolve" the value by de-referencing
-    // the context variable whose name appears between the curlies.
-    private String resolvePropertyValue(String spec, MessageContext msgCtxt) {
-        Matcher matcher = variableReferencePattern.matcher(spec);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, "");
-            sb.append(matcher.group(1));
-            Object v = msgCtxt.getVariable(matcher.group(2));
-            if (v != null){
-                sb.append((String) v );
-            }
-            sb.append(matcher.group(3));
-        }
-        matcher.appendTail(sb);
-        return sb.toString();
-    }
-
-    public ExecutionResult execute (final MessageContext msgCtxt, final ExecutionContext execContext) {
-        try {
-            String weightsConfig = getWeights(msgCtxt);
-            WeightedRandomSelector wrs = selectorCache.get(weightsConfig);
-            // set a variable.
-            msgCtxt.setVariable(varName("bucket"), Integer.toString(wrs.select()));
-        }
-        catch (Exception e) {
-            if (getDebug()) {
-                System.out.println(ExceptionUtils.getStackTrace(e));
-            }
-            String error = e.toString();
-            msgCtxt.setVariable(varName("exception"), error);
-            int ch = error.lastIndexOf(':');
-            if (ch >= 0) {
-                msgCtxt.setVariable(varName("error"), error.substring(ch+2).trim());
-            }
-            else {
-                msgCtxt.setVariable(varName("error"), error);
-            }
-            msgCtxt.setVariable(varName("stacktrace"), ExceptionUtils.getStackTrace(e));
-            return ExecutionResult.ABORT;
-        }
-
-        return ExecutionResult.SUCCESS;
-    }
+    return ExecutionResult.SUCCESS;
+  }
 }
